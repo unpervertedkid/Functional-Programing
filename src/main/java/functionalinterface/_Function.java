879b22386972be107ac748d19b1411e85a7b6f10
unpervertedkid <unpervertedkid@gmail.com>
2022-03-17 14:58:20 +0300

    Chained 2 functions together

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -11,9 +11,20 @@ public class _Function {
         //Using a function
        int increment2 =  incrementByOneFunction.apply(1);
         System.out.println(increment2);
+
+        int multiply = multiplyby10Function.apply(increment2);
+        System.out.println(multiply);
+
+        //Chain the functions
+        Function<Integer, Integer> addByOneAndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyby10Function);
+
+        System.out.println(addByOneAndThenMultiplyBy10.apply(4));
+
     }
+
     //Function
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+    static Function<Integer, Integer> multiplyby10Function = number -> number * 10;
 
     //Method to increment number by 1
     static int incrementByOne(int number) {
